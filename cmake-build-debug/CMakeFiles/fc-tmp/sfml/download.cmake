835fe1f603d185d1c5be93e0e71efa31f7923089
Jakub Sekuła <kuba.sekula21@gmail.com>
2025-04-22 18:22:33 +0200

    Przeniesienie folderow assets i fonts poza cmake-build-debug

--- cmake-build-debug/CMakeFiles/fc-tmp/sfml/download.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION 3.29)
-
-message(VERBOSE "Executing download step for sfml")
-
-block(SCOPE_FOR VARIABLES)
-
-include("C:/projekty/cpp/MCDU/cmake-build-debug/CMakeFiles/fc-tmp/sfml/sfml-gitclone.cmake")
-
-endblock()
